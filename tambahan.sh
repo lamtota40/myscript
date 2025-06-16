a26736edd1c2269492f7f54da0ead0569a4fc01f
lamtota40 <lamtota40@gmail.com>
2025-06-16 14:12:25 +0700

    Update tambahan.sh

--- tambahan.sh
+++ tambahan.sh
@@ -5,9 +5,18 @@ sudo apt upgrade -y
 sudo parted /dev/vda --script mkpart primary ext4 20GB 40GB
 sudo parted /dev/vda --script mkpart primary linux-swap 40GB 100%
 
-mkpart primary ext4 20GB 40GB
-mkpart primary linux-swap 40GB 100%
+sudo partprobe /dev/vda
+sleep 2
 
 sudo mkfs.ext4 -L datafs /dev/vda2
 sudo mkswap /dev/vda3
 sudo swapon /dev/vda3
+
+UUID_EXT4=$(blkid -s UUID -o value "$PART_EXT4")
+UUID_SWAP=$(blkid -s UUID -o value "$PART_SWAP")
+
+mkdir -p /data
+echo "UUID=$UUID_EXT4 /data ext4 defaults 0 2" >> /etc/fstab
+echo "UUID=$UUID_SWAP none swap sw 0 0" >> /etc/fstab
+
+mount /dev/vda3 /data
