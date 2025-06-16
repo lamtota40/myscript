503b5bc95594acf9f45ab3ac1bc1171cdc6d71df
lamtota40 <lamtota40@gmail.com>
2025-06-16 15:59:04 +0700

    Update tambahan.sh

--- tambahan.sh
+++ tambahan.sh
@@ -19,4 +19,12 @@ mkdir -p /data
 echo "UUID=$UUID_EXT4 /data ext4 defaults 0 2" >> /etc/fstab
 echo "UUID=$UUID_SWAP none swap sw 0 0" >> /etc/fstab
 
+$swappiness=60
+$vfs_cache_pressure=100
+sed -i -e '/^vm.swappiness/d' -e '/^vm.vfs_cache_pressure/d' /etc/sysctl.conf
+echo -e "vm.swappiness=$swappiness\nvm.vfs_cache_pressure=$vfs_cache_pressure" >> /etc/sysctl.conf
+sysctl vm.swappiness=$swappiness
+sysctl vm.vfs_cache_pressure=$vfs_cache_pressure
+sysctl -p
+
 mount /dev/vda2 /data
