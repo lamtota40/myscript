5d557128fc61864d48ae97ed5467916ef04fead0
lamtota40 <lamtota40@gmail.com>
2025-06-16 13:01:45 +0700

    Update tambahan.sh

--- tambahan.sh
+++ tambahan.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
+sudo parted /dev/vda --script mkpart primary ext4 20GB 40GB
+sudo parted /dev/vda --script mkpart primary linux-swap 40GB 100%
+
+mkpart primary ext4 20GB 40GB
+mkpart primary linux-swap 40GB 100%
+
+sudo mkfs.ext4 -L datafs /dev/vda2
+sudo mkswap /dev/vda3
+sudo swapon /dev/vda3
