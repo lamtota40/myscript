43c63ea9f9d29729bdb7084f14edcab1c1dbe0e0
lamtota40 <lamtota40@gmail.com>
2025-06-17 11:01:05 +0700

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -6,7 +6,7 @@ cat > /etc/fail2ban/jail.d/ssh.local <<'EOF'
 enabled = true
 port = 22
 logpath = /var/log/auth.log
-maxretry = 2
+maxretry = 1
 findtime = 86400
 bantime = -1
 EOF
@@ -22,7 +22,7 @@ enabled = true
 port = 5901
 filter = tigervnc
 logpath = /root/.vnc/*.log
-maxretry = 2
+maxretry = 1
 findtime = 86400
 bantime = -1
 EOF
