b1e2d466e31c592113b62762ddf31da8ca48cf3a
lamtota40 <lamtota40@gmail.com>
2025-06-17 09:12:14 +0700

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -1,14 +1,16 @@
 sudo apt update
 sudo apt install fail2ban -y
 sudo systemctl status fail2ban
-sudo nano /etc/fail2ban/jail.d/ssh.local
+cat > /etc/fail2ban/jail.d/ssh.local <<'EOF'
 [sshd]
 enabled = true
 port = ssh
 logpath = /var/log/auth.log
 maxretry = 3
 findtime = 300
-bantime = 600
+bantime = -1
+EOF
+
 sudo systemctl restart fail2ban
 sudo fail2ban-client status
 sudo fail2ban-client status sshd
